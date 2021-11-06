41fc6de4d243c4cf887241fbb71a8e744a90ff69
Blackdog7777 <69210104+Blackdog7777@users.noreply.github.com>
2021-11-06 21:01:19 +0300

    v.2
    Бот-заметки

--- main.py
+++ main.py
@@ -1,41 +1,87 @@
 import telebot
-from random import randint
+import sqlite3
 import config as cfg
-import requests
 import json
 
 client = telebot.TeleBot(cfg.token)
 
+connection = sqlite3.connect('notes.db')
+cursor = connection.cursor()
 
-@client.message_handler(commands=['start'])
-def start(message):
-    print('[LOG] Used command `start`')
-    client.send_message(message.chat.id, 'Бот может выводить случайные картинки по запросу /image или /random_image\nПосле команды можно указать что конкретно вы хотите найти (/pictures)')
+cursor.execute("""CREATE TABLE IF NOT EXISTS notes(
+        uid INT,
+        nid INT,
+        message TEXT
+)""")
+
+connection.commit()
 
 
-@client.message_handler(commands=['pictures'])
-def pictures(message):
-    print('[LOG] Used command `pictures`')
-    client.send_message(message.chat.id, 'Список всех доступных изображений:\ndog\ncat\npanda\nfox\nkoala\nbird\nraccoon\nkangaroo')
+@client.message_handler(commands=['add_note', 'an'])
+def add_note(message):
+    content = message.text.split()
+    nid = int(content[1])
+    text = str(content[2])
+    print('[LOG] Used command `add_note`')
+    loc_connection = sqlite3.connect('notes.db')
+    loc_cursor = loc_connection.cursor()
+    loc_cursor.execute(f"SELECT * FROM notes WHERE uid={message.chat.id}")
+    msg = loc_cursor.fetchall()
+    trigger = 1
+    for i in msg:
+        if i[1] == nid:
+            trigger = 0
+            client.send_message(message.chat.id, 'Запись с таким ID уже существует')
+            break
+    if trigger:
+        loc_cursor.execute(f"INSERT INTO notes (uid, nid, message) VALUES ({message.chat.id}, {nid}, '{text}')")
+        loc_connection.commit()
+        client.send_message(message.chat.id, 'Запись успешно добавлена в журнал')
 
 
-@client.message_handler(commands=['random_image', 'ri', 'image'])
-def image(message):
-    params = message.text
-    print(f'[LOG] Used command `image` with args: {params}')
-    if len(params.split(' ')) == 1:
-        arg = 'dog'
-    else:
-        arg = params.split(' ')[1].lower()
-    url = 'https://some-random-api.ml/img/' + str(arg)
-    response = requests.get(url)
-    if response.status_code != 404:
-        text = response.text
-        context = json.loads(text)
-        img = context['link']
-        client.send_photo(message.chat.id, img)
-    else:
-        client.send_message(message.chat.id, 'Данное фото не найдено\nСписок доступных изображений: /pictures')
+@client.message_handler(commands=['view_note', 'vn'])
+def add_note(message):
+    print('[LOG] Used command `view_note`')
+    loc_connection = sqlite3.connect('notes.db')
+    loc_cursor = loc_connection.cursor()
+    loc_cursor.execute(f"SELECT * FROM notes WHERE uid={message.chat.id}")
+    msg = loc_cursor.fetchall()
+    ans1 = []
+    ans2 = []
+    for i in msg:
+        ans1.append(str(i[1]))
+        print(i[1])
+        ans2.append(str(i[2]))
+    ans = ''
+    for i in range(len(msg)):
+        ans += str(ans1[i])
+        ans += ': '
+        ans += ans2[i]
+        ans += '\n'
+    if len(msg) == 0:
+        ans = 'Записей пока нет...'
+    client.send_message(message.chat.id, 'Лист записей:\n'
+                                         f'{ans}')
+
+
+@client.message_handler(commands=['delete_note', 'dn'])
+def delete_note(message):
+    content = message.text.split()
+    nid = int(content[1])
+    print('[LOG] Used command `delete_note`')
+    loc_connection = sqlite3.connect('notes.db')
+    loc_cursor = loc_connection.cursor()
+    loc_cursor.execute(f"SELECT * FROM notes WHERE uid={message.chat.id}")
+    msg = loc_cursor.fetchall()
+    trigger = 0
+    for i in msg:
+        if i[1] == nid:
+            trigger = 1
+            loc_cursor.execute(f"DELETE FROM notes WHERE uid={message.chat.id} AND nid={nid}")
+            client.send_message(message.chat.id, 'Запись успешно удалена')
+            loc_connection.commit()
+    if not trigger:
+        client.send_message(message.chat.id, 'Запись с таким ID не существует')
 
 
 client.polling(none_stop=True, interval=0)
